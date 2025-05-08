881b3ad789f637f5b4a9fb0424937a6c62f7fee0
PRbrate <pauloteles08@hotmail.com>
2025-05-07 21:35:59 -0300

    Feat : Profile v1

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
-import { Input, Flex, Button, Center, Text } from "@chakra-ui/react";
+import { Input, Flex, Heading, Box, Text, Button } from "@chakra-ui/react";
 import { SideBar } from "@/components/sidebar";
+import Link from "next/link";
+
 
 export default function Profile() {
   return (
@@ -11,11 +13,123 @@ export default function Profile() {
       <SideBar>
         <Flex
           background={"barber.100"}
-          height={"100vh"}
-          align={"center"}
-          justifyContent={"center"}
+          direction="column"
+          alignItems="flex-start"
+          justifyContent="flex-start"
         >
-          <Text color={"button.cta"} fontSize={30}> Perfil</Text>
+          <Flex
+            w="100%"
+            direction="row"
+            alignItems="center"
+            justifyContent="flex-start"
+          >
+            <Heading
+              fontSize="4xl"
+              fontFamily="inherit"
+              marginTop={4}
+              marginRight={4}
+              marginBottom={4}
+              color={"barber.400"}
+            >
+              Minha Conta
+            </Heading>
+          </Flex>
+
+          <Flex
+            maxWidth="700px"
+            background="barber.50"
+            width="100%"
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius={10}
+          >
+            <Flex
+              paddingTop={8}
+              paddingBottom={8}
+              direction="column"
+              width="85%"
+            >
+              <Text
+                marginBottom={2}
+                fontSize="2xl"
+                fontWeight="bold"
+                fontFamily="inherit"
+                color="barber.100"
+              >
+                NOME DA BARBEARIA:
+              </Text>
+              <Input
+                width="100%"
+                background="barber.100"
+                placeholder="Nome da sua companhia"
+                size="lg"
+                type="Text"
+              />
+
+              <Text
+                paddingTop={4}
+                marginBottom={2}
+                fontSize="2xl"
+                fontWeight="bold"
+                fontFamily="inherit"
+                color="barber.100"
+              >
+                ENDEREÇO:
+              </Text>
+              <Input
+                width="100%"
+                background="barber.100"
+                placeholder="Endereço da barbearia"
+                size="lg"
+                type="Text"
+              />
+
+              <Text
+                paddingTop={4}
+                marginBottom={2}
+                fontSize="2xl"
+                fontWeight="medium"
+                fontFamily="inherit"
+                color="barber.100"
+              >
+                PLANO ATUAL:
+              </Text>
+
+              <Flex
+                padding={2}
+                width="100%"
+                border="4px"
+                rounded={10}
+                borderColor="barber.900"
+                backgroundColor="barber.900"
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Text color="green.300" fontSize="2x1" fontWeight="extrabold">
+                  Plano Gratis
+                </Text>
+
+                <Link href="/planos">
+                  <Box
+                    color="barber.100"
+                    fontSize="2x1"
+                    padding={1.5}
+                    rounded={7}
+                    fontWeight="bold"
+                    backgroundColor="green.700"
+                  >
+                    Mudar de plano
+                  </Box>
+                </Link>
+              </Flex>
+
+              <Button backgroundColor="button.default"  _hover={{ bg : "green.700"}} marginTop={3} width= "100%" padding={6} textColor="barber.900" > Salvar </Button>
+
+              <Button backgroundColor="gray.400" _hover={{ bg :"gray.400"}} borderWidth={2} borderColor="barber.400" marginTop={3} width= "100%" textColor="barber.900">  Sair da Conta</Button>
+            </Flex>
+          </Flex>
         </Flex>
       </SideBar>
     </>
